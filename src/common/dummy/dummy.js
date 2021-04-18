9a7945a37bcd8f4dc17b4127d890d8d5c2e45519
brayaneider1 <b.alvarez@udla.edu.co>
2021-04-17 21:39:58 -0500

    feat(Test):100%

--- src/common/dummy/dummy.js
+++ src/common/dummy/dummy.js
@@ -7,6 +7,16 @@ export const dummy = {
             ipAddress:'192.212.174.101',
             location:'Brooklyn,NY 1001',
             timezone:'UTC-05:00',
-            isp:'SpaceX Starlink'
+            isp:'SpaceX Starlink',
+            posistion:[33.4095234, -79.7487981]
+        },
+        {
+            id:'1',
+            ipAddress:'294.654.174.101',
+            location:'West Coust,Los Angeles',
+            timezone:'UTC-05:00',
+            isp:'Ice Cub House',
+            posistion:[34.0207305,-118.6919144]
+
         },
     ]}
